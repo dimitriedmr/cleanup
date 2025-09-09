964623d9bd60ea0c9ba0a0b2e25c19802788be76
dimitriedmr <panainte.dimitrie@gmail.com>
2025-09-10 02:08:38 +0300

    speed increase fix

--- compare-folders.py
+++ compare-folders.py
@@ -1,15 +1,18 @@
-#using multiprocessing, it computes hash of files from given directories, and it saves them to a csv file
+# using multiprocessing, it computes hash of files from given directories, and it saves them to a csv file
 from argparse import ArgumentParser
 from csv import writer
 from hashlib import md5
 from multiprocessing import Process, Queue, Manager, cpu_count
 from os import listdir
-from os.path import join, isdir, exists, abspath, dirname
+from os.path import join, isdir, exists, abspath, dirname, getsize
 from sys import argv
 from time import sleep
+from random import sample
+from math import floor
 
 kbit = 1024
-file_chunk_size = 8 * kbit # kByte
+file_chunk_size = 8 * kbit  # kByte
+
 
 def compute(path, queue, all_files_dict):
     """Worker function to compute files hash.
@@ -24,19 +27,19 @@ def compute(path, queue, all_files_dict):
             else:
                 # to compute file hash, use several chunks from it
                 hash = md5()
-                with open(file_path, 'rb') as f:
-                    chunk_flag = 1 # skip some chunks to increase speed
+                # file_chunks = int(floor(getsize(file_path) / file_chunk_size))
+                with open(file_path, "rb") as f:
                     chunk = f.read(file_chunk_size)
-                    while chunk: # first chunks determines hash value
-                        if chunk_flag:
+                    chunk_index = 1
+                    while chunk:
+                        if chunk_index % 2 == 0:  # take 50% of chunks; 25% not working!
                             hash.update(chunk)
                         chunk = f.read(file_chunk_size)
-                        chunk_flag = not chunk_flag
-
+                        chunk_index += 1
                 hash = hash.hexdigest()
-                
+
                 # update dict, merge based on keys
-                value = file_path.encode('utf-8').decode('utf-8')
+                value = file_path.encode("utf-8").decode("utf-8")
                 if hash in all_files_dict:
                     all_files_dict[hash] += [value]
                 else:
@@ -45,48 +48,53 @@ def compute(path, queue, all_files_dict):
         print(f"Can't access directory: {e}")
         pass
 
+
 def worker(queue, all_files_dict):
     done = False
     while not done:
         if queue.empty():
-            sleep(1) # 1s
+            sleep(1)  # 1s
         if queue.empty():
             done = True
         else:
             compute(queue.get(), queue, all_files_dict)
 
-def save(files_path:list):
+
+def save(files_path: list):
     # save results dictionary to csv
     out_path = join(dirname(abspath(__file__)), r"output.csv")
-    sep = '.'
+    sep = "."
     i = 0
     while exists(out_path):
-        p,ext = out_path.split(sep)
+        p, ext = out_path.split(sep)
         out_path = p + str(i) + sep + ext
     print(out_path)
 
-    with open(out_path, mode="w", newline="", encoding='utf8') as file:
+    with open(out_path, mode="w", newline="", encoding="utf8") as file:
         w = writer(file)
         w.writerow(["checksum", "paths"])
         for key, value in files_path.items():
             w.writerow([key, value])
 
+
 if __name__ == "__main__":
-    
-    parser = ArgumentParser(description="Check for duplicates for a given list of folders")
+
+    parser = ArgumentParser(
+        description="Check for duplicates for a given list of folders"
+    )
     parser.add_argument("-d", action="extend", nargs="+", type=str)
     args = parser.parse_args()
 
     if len(argv) <= 1:
-        print("It needs a directory as argument. E.g. \"-d C:\\\"")
+        print('It needs a directory as argument. E.g. "-d C:\\"')
     else:
         # shared queue with directories to explore
-        queue = Queue() 
+        queue = Queue()
         for path in args.d:
             queue.put(path)
         # need a manager to share results dictionary
         manager = Manager()
-        results = manager.dict() 
+        results = manager.dict()
         # create workers
         processes = []
         for _ in range(cpu_count()):
